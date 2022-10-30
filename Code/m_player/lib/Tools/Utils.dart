f959d2328c5a3930a5acfe29407c1f99ca547e82
Sky <Sky.Rivera89@gmail.com>
2022-10-31 00:44:24 +0330

    Add Custom Colors

---
+++ Code/m_player/lib/Tools/Utils.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class myColors {
+
+  static Color green = Color(0xFF05595B);
+  static Color yellow = Color(0xFFE2D784);
+  static Color darkGreen = Color(0xFF062C30);
+  static Color wihte = Color(0xFFF5F5F5);
+
+}
