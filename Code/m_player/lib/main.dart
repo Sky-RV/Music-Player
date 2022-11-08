2fa01f8226060be527771cf9b59c9f17ad2c0f85
Sky <Sky.Rivera89@gmail.com>
2022-11-08 05:11:05 +0330

    Add Log In Page

--- Code/m_player/lib/main.dart
+++ Code/m_player/lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:m_player/UI/LogIn-SignIn/LogIn.dart';
 import 'package:m_player/UI/Main_Page.dart';
 
 void main() {
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Main_Page(),
+      home: LogIn(),
     )
   );
 }
