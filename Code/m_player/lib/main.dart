8fac4221e0efb9826ae567ad5190a538069e1f6f
Sky <Sky.Rivera89@gmail.com>
2023-01-16 20:31:08 +0330

    Refactors
    Change File Names

--- Code/m_player/lib/main.dart
+++ Code/m_player/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:m_player/UI/LogIn-SignIn/LogIn.dart';
+import 'package:m_player/UI/LogIn-SignIn/SignIn_Page.dart';
 import 'package:m_player/UI/Main_Page.dart';
 
 void main() {
