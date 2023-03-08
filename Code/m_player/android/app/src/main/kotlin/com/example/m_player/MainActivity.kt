d34a5407feaabf15fad2f4410a40d5684e50303c
Sky <Sky.Rivera89@gmail.com>
2023-03-08 21:28:05 +0330

    Restart Project

--- Code/m_player/android/app/src/main/kotlin/com/example/m_player/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.m_player
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- Code/m_player/lib/Models/Album.dart
+++
@@ -1,9 +0,0 @@
-class Album {
-
-  String? ID;
-  String? Title;
-  String? Artist;
-  String? Icon;
-
-  Album(this.ID, this.Title, this.Artist, this.Icon);
-}
