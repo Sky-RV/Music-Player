7d08827602d3b99115a728fdad5103b98afe8323
Sky <Sky.Rivera89@gmail.com>
2023-03-08 23:56:44 +0330

    Floating Navigator Bar

--- Code/m_player/lib/UI/Screens/Dashboard/Dashboard_Screen.dart
+++ Code/m_player/lib/UI/Screens/Dashboard/Dashboard_Screen.dart
@@ -1,4 +1,9 @@
+import 'package:floating_bottom_navigation_bar/floating_bottom_navigation_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:m_player/UI/Screens/Category/Category_Screen.dart';
+import 'package:m_player/UI/Screens/Favorite/Favorite_Screen.dart';
+import 'package:m_player/UI/Screens/Home/Home_Screen.dart';
+import 'package:m_player/Utils/MyColors.dart';
 
 class DashboardScreen extends StatefulWidget {
   const DashboardScreen({Key? key}) : super(key: key);
@@ -8,8 +13,57 @@ class DashboardScreen extends StatefulWidget {
 }
 
 class _DashboardScreenState extends State<DashboardScreen> {
+
+  int _currentIndex = 0;
+  List<Widget> bodyScreens = [
+    HomeScreen(),
+    CategoryScreen(),
+    FavoriteScreen()
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Music Player", style: TextStyle(color: myColors.darkGreen)),
+        backgroundColor: myColors.white,
+        centerTitle: true,
+      ),
+
+      extendBody: true,
+
+      backgroundColor: myColors.white,
+
+      body:   bodyScreens.elementAt(_currentIndex),
+
+      bottomNavigationBar: FloatingNavbar(
+        onTap: (value){
+          setState(() {
+            _currentIndex = value;
+          });
+        },
+        currentIndex: _currentIndex,
+        backgroundColor: myColors.white,
+        selectedItemColor: myColors.green,
+        selectedBackgroundColor: myColors.white,
+        unselectedItemColor: Colors.black,
+        items: [
+          FloatingNavbarItem(
+            icon: Icons.home,
+            title: "Home"
+          ),
+
+          FloatingNavbarItem(
+              icon: Icons.category,
+              title: "Category"
+          ),
+
+          FloatingNavbarItem(
+              icon: Icons.favorite,
+              title: "Favorites"
+          )
+        ],
+      ),
+    );
   }
 }
---
+++ Code/m_player/lib/Utils/MyColors.dart
@@ -0,0 +1,11 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+
+class myColors {
+
+  static Color green = Color(0xFF05595B);
+  static Color yellow = Color(0xFFE2D784);
+  static Color darkGreen = Color(0xFF062C30);
+  static Color white = Color(0xFFF5F5F5);
+
+}
